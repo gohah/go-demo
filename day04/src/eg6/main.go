0b6c3b9f3a9ffd7871ac57569c18068b30224efa
huangwei <huangwei@liechengcf.com>
2017-06-20 12:06:00 +0800

    day04 eg6

---
+++ day04/src/eg6/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+	"fmt"
+	"log"
+	"strconv"
+)
+
+//go模板标签使用
+
+func testTemplate(w http.ResponseWriter, r * http.Request) {
+
+	tmpl, err := template.ParseFiles("eg6/main.html")
+
+	if err != nil {
+		fmt.Fprintf(w, "ParseFiles: %v", err)
+		return
+	}
+
+	score := r.FormValue("score")
+	scored,_ := strconv.Atoi(score)
+
+	err = tmpl.Execute(w,map[string]interface{}{
+		"score":scored,
+		"request":r,
+		"arr":[]int{1,2,3,4,5},
+	})
+
+	if err != nil {
+		fmt.Fprintf(w, "Execute: %v",err)
+		return
+	}
+}
+
+
+func main() {
+	http.HandleFunc("/", testTemplate)
+
+	log.Println("start v4...")
+
+	log.Fatal(http.ListenAndServe(":4000",nil))
+}
