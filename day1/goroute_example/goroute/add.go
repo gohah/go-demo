d6c2d2aa7334faccef70494aad2e1031141a347c
huangwei <huangwei@liechengcf.com>
2017-08-10 08:53:13 +0800

    delete all

--- day1/goroute_example/goroute/add.go
+++
@@ -1,8 +0,0 @@
-package goroute
-
-
-func Add(a int, b int, c chan int) {
-
-	sum := a +b
-	c <- sum
-}
--- day8/test/calc.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func add(a, b int) int {
-	return a + b
-}
-
-func sub(a, b int) int {
-	return a - b
-}
